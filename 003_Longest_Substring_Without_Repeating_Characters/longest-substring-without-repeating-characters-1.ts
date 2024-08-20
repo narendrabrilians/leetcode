cabcaefeeb349b4ef3c2a82430ccb05e620be4b4
Narendra Febriliansyah R <narendrabrilians@gmail.com>
2024-08-20 13:00:32 +0700

    chore: reuploud solution for 'longest substring without repeating characters' problem

---
+++ 003_Longest_Substring_Without_Repeating_Characters/longest-substring-without-repeating-characters-1.ts
@@ -0,0 +1,23 @@
+function lengthOfLongestSubstring(s: string): number {
+  if (s.length === 1) {
+    return 1;
+  }
+  let longestLength = 0;
+  let currentLength = 0;
+  let objectChecker: { [index: string]: number } = {};
+  for (let i = 0; i < s.length; i++) {
+    if (objectChecker[s[i]] === undefined) {
+      currentLength += 1;
+      objectChecker[s[i]] = i;
+      if (longestLength < currentLength) {
+        longestLength = currentLength;
+      }
+    } else {
+      i = objectChecker[s[i]] + 1;
+      objectChecker = {};
+      objectChecker[s[i]] = i;
+      currentLength = 1;
+    }
+  }
+  return longestLength;
+}
